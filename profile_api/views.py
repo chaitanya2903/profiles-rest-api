2471df21cd334dc410ad87574bd9e81ced713368
beecee <sbachhav45@gmail.com>
2020-03-02 16:26:58 +0530

    added put,path and delete methods

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from profile_api import serializers
+from rest_framework import status
 
 class HelloAPIView(APIView):
     """ Test APIView """
+    serializer_class  = serializers.HelloSerializer
     def get(self, request, format = None):
         """ returns a list of APIView features """
         an_api_view = [
@@ -13,3 +15,29 @@ class HelloAPIView(APIView):
         'is mapped to URLS'
         ]
         return Response({'message':"Hello!",'an_apiview' : an_api_view})
+
+    def post(self,request):
+        """ create a hello message with name"""
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            msg = "Hello {}".format(name)
+            return Response({'message':msg})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST,
+            )
+
+    def put(self,request,pk = None):
+        """ Handle updating an object """
+        return Response({"method": 'PUT'})
+
+    def patch(self,request, pk = None):
+        """ handle a partial update of an object """
+        return Response({"method" : 'PATCH'})
+
+    def delete(self,request,pk = None):
+        """ delete an object """
+        return Response({"method" : "DELETE"})
+        
